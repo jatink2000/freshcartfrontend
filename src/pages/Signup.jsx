04d406be06e4939ce946d4bcb1b11eaec1289306
jatink2000 <jatink8059@gmail.com>
2025-10-08 12:15:21 +0530

    djh

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,32 +1,48 @@
 import axios from 'axios'
 import React, { useState } from 'react'
+import Swal from 'sweetalert2'
 
 function Signup() {
 
-   let [signup,setsignup]= useState({})
+  let [signup, setsignup] = useState({})
 
-   let inputvalue=()=>{
+  let inputvalue = (e) => {
     setsignup({
-       ...signup,[ e.target.name]:e.target.value
+      ...signup, [e.target.name]: e.target.value
     })
-   }
+  }
 
 
-   let submitbtn=()=>{
-    axios.post("http://localhost:5000/signup",{signup})
-   }
+  let submitbtn = () => {
+    axios.post("http://localhost:5000/signup", { signup }).then((res) => {
+      if (res.data.status) {
+        Swal.fire({
+          title: "signup success",
+          icon: "success"
+        });
+      }
+      else {
+        Swal.fire({
+          icon: "error",
+          title: "failed to success",
+        });
+      }
+    }).catch((err) => {
+      console.log(err)
+    })
+  }
 
 
 
 
   return (
     <>
-    <h1>signup component</h1>
+      <h1>signup component</h1>
 
-    <input type='email' placeholder='enter your email' name='email' onChange={inputvalue}/>
-    <input type='password' placeholder='enter your password' name='password' onChange={inputvalue}/>
-    <button onClick={submitbtn}>signup</button>
-      
+      <input type='email' placeholder='enter your email' name='email' onChange={inputvalue} />
+      <input type='password' placeholder='enter your password' name='password' onChange={inputvalue} />
+      <button onClick={submitbtn}>signup</button>
+
     </>
   )
 }
